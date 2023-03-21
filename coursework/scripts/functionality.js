4e0cfaecf5b1c4ecb4c6e9fc418e9d1775127e36
Scott-S34 <115486178+Scott-S34@users.noreply.github.com>
2023-03-21 14:37:22 +0000

    Add files via upload

--- coursework/scripts/functionality.js
+++ coursework/scripts/functionality.js
@@ -19,57 +19,130 @@ function login(){
 
 
 
-document.getElementById("jokers-tour").addEventListener("mouseover", highlight);
-document.getElementById("jokers-tour").addEventListener("mouseout", delight);
+document.getElementById("jokers-tour").addEventListener("mouseover", highlight1);
+document.getElementById("jokers-tour").addEventListener("mouseout", delight1);
 
-document.getElementById("season-4").addEventListener("mouseover", highlight);
-document.getElementById("season-4").addEventListener("mouseout", delight);
 
-document.getElementById("no-Joe").addEventListener("mouseover", highlight);
-document.getElementById("no-Joe").addEventListener("mouseout", delight);
+document.getElementById("season-4").addEventListener("mouseover", highlight2);
+document.getElementById("season-4").addEventListener("mouseout", delight2);
 
-document.getElementById("suits").addEventListener("mouseover", highlight);
-document.getElementById("suits").addEventListener("mouseout", delight);
 
-document.getElementById("Q-meme").addEventListener("mouseover", highlight);
-document.getElementById("Q-meme").addEventListener("mouseout", delight);
+document.getElementById("no-Joe").addEventListener("mouseover", highlight3);
+document.getElementById("no-Joe").addEventListener("mouseout", delight3);
 
-document.getElementById("cats").addEventListener("mouseover", highlight);
-document.getElementById("cats").addEventListener("mouseout", delight);
 
-document.getElementById("ferret").addEventListener("mouseover", highlight);
-document.getElementById("ferret").addEventListener("mouseout", delight);
+document.getElementById("suits").addEventListener("mouseover", highlight4);
+document.getElementById("suits").addEventListener("mouseout", delight4);
 
-document.getElementById("larry").addEventListener("mouseover", highlight);
-document.getElementById("larry").addEventListener("mouseout", delight);
 
-document.getElementById("sal-glory").addEventListener("mouseover", highlight);
-document.getElementById("sal-glory").addEventListener("mouseout", delight);
+document.getElementById("Q-meme").addEventListener("mouseover", highlight5);
+document.getElementById("Q-meme").addEventListener("mouseout", delight5);
+
+
+document.getElementById("cats").addEventListener("mouseover", highlight6);
+document.getElementById("cats").addEventListener("mouseout", delight6);
+
+
+document.getElementById("ferret").addEventListener("mouseover", highlight7);
+document.getElementById("ferret").addEventListener("mouseout", delight7);
+
+document.getElementById("larry").addEventListener("mouseover", highlight8);
+document.getElementById("larry").addEventListener("mouseout", delight8);
+
+
+document.getElementById("sal-glory").addEventListener("mouseover", highlight9);
+document.getElementById("sal-glory").addEventListener("mouseout", delight9);
 
 
 
 function highlight1(){
-    document.getElementById("jokers-tour").style = "border: solid blue 2px;"
-    document.getElementById("season-4").style = "border: solid blue 2px;"
-    document.getElementById("no-Joe").style = "border: solid blue 2px;"
-    document.getElementById("suits").style = "border: solid blue 2px;"
-    document.getElementById("Q-meme").style = "border: solid blue 2px;"
-    document.getElementById("cats").style = "border: solid blue 2px;"
-    document.getElementById("ferret").style = "border: solid blue 2px;"
-    document.getElementById("larry").style = "border: solid blue 2px;"
-    document.getElementById("sal-glory").style = "border: solid blue 2px;"
+    document.getElementById("jokers-tour").style = "border: dotted purple 6px;"
+
+}
+
+function highlight2(){
+    document.getElementById("season-4").style = "border: dotted purple 6px;"
+}
+
+function highlight3(){
+    document.getElementById("no-Joe").style = "border: dotted purple 6px;"
+}
+
+function highlight4(){
+    document.getElementById("suits").style = "border: dotted purple 6px;"
+}
+
+function highlight5(){
+    document.getElementById("Q-meme").style = "border: dotted purple 6px;"
+}
+
+
+function highlight6(){
+    document.getElementById("cats").style = "border: dotted purple 6px;"
+}
+
+function highlight7(){
+    document.getElementById("ferret").style = "border: dotted purple 6px;"
+}
+function highlight8(){
+    document.getElementById("larry").style = "border: dotted purple 6px;"
+}
+
+function highlight9(){
+    document.getElementById("sal-glory").style = "border: dotted purple 6px;"
 
 }
 
 
-function delight() {
+
+
+
+
+function delight1() {
     document.getElementById("jokers-tour").style = "border: none";
+}
+
+
+
+function delight2() {
     document.getElementById("season-4").style = "border: none"
+}
+
+
+
+function delight3() {
     document.getElementById("no-Joe").style = "border: none"
+}
+
+
+
+function delight4() {
     document.getElementById("suits").style = "border: none"
+}
+
+
+function delight5() {
     document.getElementById("Q-meme").style = "border: none"
+}
+
+function delight6() {
     document.getElementById("cats").style = "border: none"
+}
+
+
+
+function delight7() {
     document.getElementById("ferret").style = "border: none"
+}
+
+
+function delight8() {
     document.getElementById("larry").style = "border: none"
+}
+
+
+function delight9() {
     document.getElementById("sal-glory").style = "border: none"
 }
+
+
